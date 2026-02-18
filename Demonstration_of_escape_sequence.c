006ae2724cf1413b2e4ce0e6db44529a49c0af74
Anmol Subedi <239953798+anmol95subedi@users.noreply.github.com>
2026-02-18 22:18:24 +0545

    Simple use of escape sentence like newline and tab

---
+++ Demonstration_of_escape_sequence.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    /*Demonstration of new line*/
+    printf("hi this is a sentence  \n this is another line \n this is second line\n");
+    /*Demonstration of tab*/
+    printf("potato \t anmol \t apples\t");
+}
