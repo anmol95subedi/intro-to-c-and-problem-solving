ae2630f671f7c4a22d363542f13f565728659212
Anmol Subedi <239953798+anmol95subedi@users.noreply.github.com>
2026-04-15 19:57:35 +0545

    First program
    These are my first programs , that i uploaded to github from my local server using terminal and git commands , It helped me learn how to connect local git to a remote git with the use of SSH Keys.

---
+++ To_find_sum_of_two_integers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int a , b , s;
+    printf("Enter an integer = ");
+    scanf("%d",&a);
+    printf("Enter another integer = ");
+    scanf("%d",&b);
+    s=a+b;
+    printf("%d + %d = %d",a,b,s);
+}
